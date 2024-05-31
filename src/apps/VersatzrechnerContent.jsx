07dc1cf49d64f5e86cf981946a1182ace76fcaa8
Noel Engeler <noel.engeler@gmail.com>
2024-05-31 18:38:27 +0200

    refactored versatzrechner.astro to use react component

---
+++ src/apps/VersatzrechnerContent.jsx
@@ -0,0 +1,120 @@
+import Select from "../components/Select"
+import Input from "../components/Input"
+
+import cutters from "../../public/cutters.json";
+
+const tools = [{ id: 1, name: "... Custom Tool" }];
+let count = 2;
+for (const [key, value] of Object.entries(cutters)) {
+  tools.push({ id: count, name: key });
+  count++;
+}
+
+const tool_types = [
+  { id: 1, name: "Fasenfräser" },
+  { id: 2, name: "Kombientgrater" },
+  { id: 3, name: "Schwalbenschwanz" },
+];
+const chamfer_types = [
+  { id: 1, name: "Regulär" },
+  { id: 2, name: "Rückwärts" },
+];
+const offset_types = [
+  { id: 1, name: "Oberseite" },
+  { id: 2, name: "Unterseite" },
+];
+
+function VersatzrechnerContent() {
+  return (
+    <>
+      <div className="py-24 px-16 w-full max-w-6xl h-full grid lg:grid-cols-2 md:grid-cols-1 gap-8">
+        <section className="flex flex-col gap-4">
+          <div className="text-center">
+            <h1 className="text-2xl font-light border-b-2 border-black px-4 py-2">
+              Werkzeug
+            </h1>
+          </div>
+          <div className="flex w-full gap-4 justify-stretch">
+            <Select options={tools} label="Werkzeug" name="tool" client:load />
+            <Select
+              options={tool_types}
+              label="Werkzeugart"
+              name="tool_type"
+              client:load
+            />
+          </div>
+          <div className="flex w-full gap-4 justify-stretch">
+            <Input label="Schneiden-⌀" identifier="d1" />
+            <Input label="Spitzen-⌀" identifier="d2" />
+            <Input label="Schaft-⌀" identifier="d3" />
+          </div>
+          <div className="flex w-full gap-4 justify-stretch">
+            <Input label="Schneidenlänge" identifier="l1" />
+            <Input label="Mittellänge" identifier="l2" />
+            <Input label="Stumpflänge" identifier="l3" />
+          </div>
+          <div className="flex w-full gap-4 justify-stretch">
+            <Input label="Spitzenwinkel" identifier="a1" />
+            <Input label="Schulterwinkel" identifier="a2" />
+          </div>
+        </section>
+        <div>
+          <img src="/debug_placeholder_img.png" alt="" />
+        </div>
+        <section className="flex flex-col gap-4">
+          <div className="text-center">
+            <h1 className="text-2xl font-light border-b-2 border-black px-4 py-2">
+              Fase
+            </h1>
+          </div>
+          <div className="flex w-full gap-4 justify-stretch">
+            <Select
+              options={chamfer_types}
+              label="Art"
+              name="chamfer_type"
+              client:load
+            />
+            <Input label="Fase Axial" identifier="Ca" />
+            <Input label="Fase Radial" identifier="Cr" />
+          </div>
+          <div className="flex w-full gap-4 justify-stretch">
+            <Select
+              options={offset_types}
+              label="Versatzrichtung"
+              name="offset_type"
+              client:load
+            />
+            <Input label="Versatz Axial" identifier="Oa" />
+            <Input label="Versatz Radial" identifier="Or" />
+          </div>
+          <div className="flex w-full justify-stretch border-b-2 border-black mt-8 mb-6"></div>
+          <div className="flex w-full gap-4 justify-around">
+            <div className="flex flex-col w-auto">
+              <p className="font-extralight">Wandaufmass:</p>
+              <p
+                id="offset-wall"
+                className="block w-full text-center py-2 bg-sky-100 border-black border-2 shadow-md"
+              >
+                -0.1
+              </p>
+            </div>
+            <div className="flex flex-col w-auto">
+              <p className="font-extralight">Bodenaufmass:</p>
+              <p
+                id="offset-floor"
+                className="block w-full text-center py-2 bg-sky-100 border-black border-2 shadow-md"
+              >
+                -1.1
+              </p>
+            </div>
+          </div>
+        </section>
+        <div>
+          <img src="/debug_placeholder_img.png" alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default VersatzrechnerContent
---
+++ src/components/Input.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+
+interface Props {
+  label: string;
+  identifier: string;
+  disabled?: boolean;
+}
+
+const NumberInput: React.FC<Props> = ({ label, identifier, disabled = false }) => {
+
+  useEffect(() => {
+    function formatNumber() {
+      const numberInput = document.getElementById(identifier) as HTMLInputElement;
+      const valueIn = numberInput.value;
+
+      if (valueIn === "") {
+        return;
+      }
+
+      const valueInArray = valueIn.split(".");
+      if (valueInArray.length > 2) {
+        alert("invalid input");
+        numberInput.value = "";
+        return;
+      }
+
+      if (valueInArray.length === 1 || valueInArray[1] === "" || parseFloat(valueInArray[1]) === 0) {
+        numberInput.value = `${valueInArray[0]}.0`;
+        return;
+      }
+
+      numberInput.value = `${parseFloat(parseFloat(valueIn).toFixed(3))}`;
+    }
+
+    const inputElement = document.getElementById(identifier);
+    inputElement.addEventListener("blur", formatNumber);
+    inputElement.addEventListener("format_number_input", formatNumber);
+
+    return () => {
+      inputElement.removeEventListener("blur", formatNumber);
+      inputElement.removeEventListener("format_number_input", formatNumber);
+    };
+  }, [identifier]);
+
+  return (
+    <div data-identifier={identifier} className="w-full">
+      <div>
+        <p className="text-black font-extralight">{label}:</p>
+        <input
+          type="text"
+          name={identifier}
+          id={identifier}
+          disabled={disabled}
+          className={`w-full border-2 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none ${disabled ? "bg-gray-200 border-gray-600 text-gray-700" : "border-black text-black"}`}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default NumberInput;
+
